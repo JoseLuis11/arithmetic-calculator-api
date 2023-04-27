78b5647e97639ff0cb4c717888dac67e753e16a3
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-04-26 18:13:00 -0600

    Removes unnecessary comment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-/* eslint @typescript-eslint/no-var-requires: 0 */  // --> OFF
+/* eslint @typescript-eslint/no-var-requires: 0 */
 const { compilerOptions } = require('./tsconfig.json');
 const { pathsToModuleNameMapper } = require('ts-jest');
 
