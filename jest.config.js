7217b02548d456d752227a2e7e01371f1ab22897
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-04-26 18:03:26 -0600

    Allow jest to use defined re-mapped paths

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+/* eslint @typescript-eslint/no-var-requires: 0 */  // --> OFF
+const { compilerOptions } = require('./tsconfig.json');
+const { pathsToModuleNameMapper } = require('ts-jest');
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
 };
