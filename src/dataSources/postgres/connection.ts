77a9604a46dbcb5d129c80f33c533cb39f95d697
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-05-08 20:44:38 -0600

    test github action with errors in build

--- src/dataSources/postgres/connection.ts
+++ src/dataSources/postgres/connection.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const postgresDataSource = new DataSource({
-  type: 'postgres',
+  type: 'postgres',asdasda
   url: accessEnv('DB_URI'),
   synchronize: false,
   logging: false,
