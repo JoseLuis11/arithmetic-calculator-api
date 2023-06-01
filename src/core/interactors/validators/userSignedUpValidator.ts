98eba72a2374030a054ea35521764f11bdcb2848
Jose Luis Castro <jluis.scooter11@hotmail.com>
2023-05-31 18:58:53 -0600

    debugging PASSWORD_MIN var

--- src/core/interactors/validators/userSignedUpValidator.ts
+++ src/core/interactors/validators/userSignedUpValidator.ts
@@ -1,13 +1,17 @@
 import Joi from 'joi';
 import accessEnv from '@utils/accessEnv';
 
+const min = accessEnv('PASSWORD_MIN');
+
+console.log('min', min)
+console.log('typeof min', typeof min)
 const userSignedUpValidator = Joi.object({
   username: Joi.string()
     .required()
     .email(),
   password: Joi.string()
     .required()
-    .min(parseInt(accessEnv('PASSWORD_MIN')))
+    .min(parseInt(min))
     .max(parseInt(accessEnv('PASSWORD_MAX')))
 });
 
